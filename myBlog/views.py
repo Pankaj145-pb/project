d8fa1c41a2af137678b4910048081cbc88e2701e
Pankaj145-pb <pankaj.ab.net@gmail.com>
2020-12-23 08:05:42 +0530

    Modified

--- myBlog/views.py
+++ myBlog/views.py
@@ -33,13 +33,6 @@ class PostDetailView(DetailView):
     model = Post
     template_name = "myBlog/post_detail.html"
 
-    def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.all()
-        context = super(IndexView, self).get_context_data(*args, **kwargs)
-        context['cat_menu'] = cat_menu
-        return context
-
-
 
 class CreatePostView(CreateView):
     model = Post
@@ -68,4 +61,5 @@ class CreateCategoryView(CreateView):
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'myBlog/delete_post.html'
-    fields = '__all__'    
+    fields = '__all__'
+    success_url = reverse_lazy('home')
